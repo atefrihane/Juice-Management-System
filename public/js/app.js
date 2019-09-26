bb64d442ffabfe0ac58b791db0ed01bbd66f4fb6
Melek Degla <melek199767@gmail.com>
2019-09-26 13:38:21 +0200

    edit stop rental

--- public/js/app.js
+++ public/js/app.js
@@ -58047,8 +58047,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\wizefresh\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\wizefresh\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/wizefresh/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/wizefresh/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
