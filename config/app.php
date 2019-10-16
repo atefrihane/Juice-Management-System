a6d8d35b3824ddc4bc1efa984698b8e0e76359bf
Atef Rihane <rihaneatef@gmail.com>
2019-10-16 17:51:42 +0100

    change app_url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://vps740184.ovh.net/wizefresh/public/'),
     'base_url'=>'hbjn',
     'asset_url' => env('ASSET_URL', null),
 
