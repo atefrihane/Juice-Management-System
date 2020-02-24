37979b56f7bc5341724f0f8cbf367e3b87ac52e1
Atef Rihane <rihaneatef@gmail.com>
2020-02-24 09:51:55 +0100

    change upload endpoint for ads

--- app/Modules/General/routes/api.php
+++ app/Modules/General/routes/api.php
@@ -10,7 +10,7 @@ Route::post('/zipcode/update/{id}','GeneralController@handleUpdateZipCode');
 Route::get('/city/{id}','GeneralController@handleGetCityZipcodes');
 Route::get('/city/companies/{id}','GeneralController@handleGetCityCompanies');
 Route::post('/city/delete/{id}','GeneralController@handleDeleteCity');
-Route::post('/api/ads/upload', 'GeneralController@handleUploadAdvertisement');
+Route::post('/api/photos/upload', 'GeneralController@handleUploadAdvertisement');
 Route::get('/api/photos/all', 'GeneralController@handleGetAllAds');
 Route::get('/api/photo/{id}', 'GeneralController@handleRemoveAdd');
 Route::get('/api/photo/name/{name}', 'GeneralController@handleRemoveAdByName');
