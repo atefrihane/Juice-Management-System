63fa217ad11f1669697995053dd8f9c99ed3daa9
Atef Rihane <rihaneatef@gmail.com>
2020-03-09 14:49:01 +0100

    try to solve pusher error

--- config/app.php
+++ config/app.php
@@ -227,7 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Batch' => Mavinoo\LaravelBatch\LaravelBatchFacade::class,
-        
+        'Pusher' => Pusher\Pusher::class,
 
     ],
 
